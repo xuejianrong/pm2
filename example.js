188d72c245f086fa21f5dd9a1369e50ced300659
xuejianrong <xuejianrong@yuntutv.net>
2018-03-08 18:43:44 +0800

    更新你妹

--- example.js
+++ example.js
@@ -4,7 +4,7 @@ const port = 3002;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World\n');
+res.end('Hello World\n你妹');
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
